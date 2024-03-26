eca3060bede3eb29fbb3031218a825768bfbc4d9
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-03-26 15:06:39 +0530

    added

---
+++ Advanced/DSA 2/Oops/package_1/Class1.java
@@ -0,0 +1,16 @@
+public class Class1 {
+
+    public int a = 1;
+    private int b = 2;
+    protected int c = 3;
+    int d = 4;
+
+    public static void main(String[] args) {
+
+        Class1 c1 = new Class1();
+        System.out.println(c1.a);
+        System.out.println(c1.b);
+        System.out.println(c1.c);
+        System.out.println(c1.d);
+    }
+}
