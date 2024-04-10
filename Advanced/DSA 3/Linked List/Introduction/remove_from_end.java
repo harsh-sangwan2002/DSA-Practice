8dd6a3b62a993a557d09876db6be0cb41ca757d3
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-04-10 20:40:17 +0530

    added

---
+++ Advanced/DSA 3/Linked List/Introduction/remove_from_end.java
@@ -0,0 +1,37 @@
+//   Definition for singly-linked list.
+class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class remove_from_end {
+
+    public ListNode removeNthFromEnd(ListNode A, int B) {
+
+        if (A == null || A.next == null)
+            return null;
+
+        ListNode fast = A;
+
+        for (int i = 1; i <= B && fast != null; i++)
+            fast = fast.next;
+
+        if (fast == null)
+            return A.next;
+
+        ListNode slow = A;
+
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+        return A;
+    }
+}
