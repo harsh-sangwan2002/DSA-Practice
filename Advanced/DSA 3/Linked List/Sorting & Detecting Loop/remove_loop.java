8439ef29172cd1df49b9650e20e22075e6021303
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-04-11 10:12:21 +0530

    added

---
+++ Advanced/DSA 3/Linked List/Sorting & Detecting Loop/remove_loop.java
@@ -0,0 +1,49 @@
+//  Definition for singly-linked list.
+
+class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class remove_loop {
+
+    public ListNode solve(ListNode A) {
+
+        ListNode slow = A, fast = A;
+        boolean flag = false;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag)
+            return A;
+
+        slow = A;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        ListNode temp = fast;
+
+        while (temp.next != fast) {
+            temp = temp.next;
+        }
+
+        temp.next = null;
+        return A;
+    }
+}
