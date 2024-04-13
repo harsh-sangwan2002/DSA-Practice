a7b458e43604d26202f15af218f283c5fb5e4223
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-04-13 20:38:11 +0530

    added

---
+++ Intermediate/DSA 1/Arrays/Prefix Sum/special_index.java
@@ -0,0 +1,49 @@
+public class special_index {
+
+    public int solve(int[] A) {
+
+        int[] even = new int[A.length];
+        int[] odd = new int[A.length];
+
+        for (int i = 0; i < A.length; i++) {
+
+            if (i == 0) {
+                even[i] = A[i];
+                odd[i] = 0;
+            }
+
+            else if (i % 2 == 0) {
+                even[i] = even[i - 1] + A[i];
+                odd[i] = odd[i - 1];
+            }
+
+            else {
+                even[i] = even[i - 1];
+                odd[i] = odd[i - 1] + A[i];
+            }
+        }
+
+        int oddSum = 0, evenSum = 0, count = 0;
+
+        for (int i = 0; i < A.length; i++) {
+
+            int st1 = 0, et1 = i - 1, st2 = i + 1, et2 = A.length - 1;
+
+            if (i == 0) {
+                oddSum = even[et2] - even[i];
+                evenSum = odd[et2] - odd[i];
+            }
+
+            else {
+
+                oddSum = odd[i - 1] + (even[et2] - even[i]);
+                evenSum = even[i - 1] + (odd[et2] - odd[i]);
+            }
+
+            if (oddSum == evenSum)
+                count++;
+        }
+
+        return count;
+    }
+}
