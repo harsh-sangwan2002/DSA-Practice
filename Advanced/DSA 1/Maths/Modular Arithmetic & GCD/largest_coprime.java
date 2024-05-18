20d7cfbf8f04f9b65cee0c612a75059df4a84b2d
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-05-18 14:15:18 +0530

    added

---
+++ Advanced/DSA 1/Maths/Modular Arithmetic & GCD/largest_coprime.java
@@ -0,0 +1,20 @@
+public class largest_coprime {
+
+    private int gcd(int a, int b) {
+
+        if (a == 0)
+            return b;
+        if (b == 0)
+            return a;
+
+        return gcd(b, a % b);
+    }
+
+    public int cpFact(int A, int B) {
+
+        while (gcd(A, B) != 1)
+            A /= gcd(A, B);
+
+        return A;
+    }
+}
