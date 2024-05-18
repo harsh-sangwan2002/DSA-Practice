42ba52cf2951b173272199d3380bbc3d0506d776
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-05-18 13:53:31 +0530

    added

---
+++ Advanced/DSA 1/Recursion/Problems/kth_symbol2.java
@@ -0,0 +1,15 @@
+public class kth_symbol2 {
+
+    public int solve(int A, long B) {
+
+        if (A == 1)
+            return 0;
+
+        int val = solve(A - 1, B / 2);
+
+        if (B % 2 == 0)
+            return val;
+
+        return 1 - val;
+    }
+}
