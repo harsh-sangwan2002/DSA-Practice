daeac2f8a48cdfaad9e0f9c4ea88e63ff54d91ef
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-05-19 13:58:11 +0530

    added

--- Advanced/DSA 2/Sorting/Count Sort & Merge Sort/inversion_count.java
+++ Advanced/DSA 2/Sorting/Count Sort & Merge Sort/inversion_count.java
@@ -1,61 +1,59 @@
 public class inversion_count {
 
-    static long mod = (long) (1e9 + 7);
+    private long mod = (long) (1e9 + 7);
 
-    private long merge(int[] A, int s, int e) {
+    private long merge(int[] arr, int mid, int s, int e) {
 
-        if (s >= e)
-            return 0L;
-
-        int mid = s + (e - s) / 2;
-        long count = 0;
-
-        count = (count + merge(A, s, mid)) % mod;
-        count = (count + merge(A, mid + 1, e)) % mod;
-        count = (count + mergeArrays(A, s, mid, e)) % mod;
-
-        return count;
-    }
-
-    private long mergeArrays(int[] A, int s, int mid, int e) {
-
-        int n1 = mid - s + 1, n2 = e - mid;
-        long count = 0L;
+        int n1 = mid - s + 1;
+        int n2 = e - mid;
 
         int[] left = new int[n1];
+        for (int i = s; i < s + n1; i++)
+            left[i - s] = arr[i];
+
         int[] right = new int[n2];
-
-        for (int i = 0; i < n1; i++)
-            left[i] = A[s + i];
-
-        for (int j = 0; j < n2; j++)
-            right[j] = A[mid + 1 + j];
+        for (int i = mid + 1; i < mid + 1 + n2; i++)
+            right[i - mid - 1] = arr[i];
 
         int i = 0, j = 0, k = s;
+        long pairs = 0L;
 
         while (i < n1 && j < n2) {
 
             if (left[i] <= right[j])
-                A[k++] = left[i++];
+                arr[k++] = left[i++];
 
             else {
-                count = (count + n1 - i) % mod;
-                A[k++] = right[j++];
+                arr[k++] = right[j++];
+                pairs = (pairs + n1 - i) % mod;
             }
         }
 
         while (i < n1)
-            A[k++] = left[i++];
+            arr[k++] = left[i++];
 
         while (j < n2)
-            A[k++] = right[j++];
+            arr[k++] = right[j++];
 
-        return count;
+        return pairs;
+    }
+
+    private long inversion(int[] arr, int s, int e) {
+
+        if (s >= e)
+            return 0L;
+
+        long pairs = 0L;
+        int mid = s + (e - s) / 2;
+        pairs = (pairs + inversion(arr, s, mid)) % mod;
+        pairs = (pairs + inversion(arr, mid + 1, e)) % mod;
+        pairs = (pairs + merge(arr, mid, s, e)) % mod;
+
+        return pairs;
     }
 
     public int solve(int[] A) {
 
-        int s = 0, e = A.length - 1;
-        return (int) merge(A, s, e);
+        return (int) (inversion(A, 0, A.length - 1) % mod);
     }
 }
